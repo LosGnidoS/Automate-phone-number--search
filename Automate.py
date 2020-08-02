976f8499c6990c3b26c641672693264a7fe898bc
Kirill Shvedov <github.shvedov@gmail.com>
2020-08-03 00:49:52 +0300

    Delete Automate.py

--- Automate.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/python3
-# created by Kirill Shvedov 
-# github: "LosGnidoS"
-
-import re 
-
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-x = phoneNumRegex.search('My phone number is 123-343-9999')
-print('Phone number found: ' + x.group())
