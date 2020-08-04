718ceb9fb4d4bec552e6cedc81423842057e657a
Kirill Shvedov <github.shvedov@gmail.com>
2020-08-04 19:54:15 +0300

    Add files via upload

---
+++ mapit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#created by KIRILL SHVEDOV
+#github: "LosGnidoS"
+
+
+'''SIMPLE AUTOMATIZATION SCRIPT
+FOR GOOGLE-MAPS SEARCH'''
+
+#1.CTRL-C SOME ADDRESS YOU WANT TO REACH
+#2.RUN THIS SCRIPT
+
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1 :
+	address = ''.join(sys.argv[1:])
+
+address = pyperclip.paste()
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
